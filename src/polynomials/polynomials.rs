03a210e8980b4de359e41ef95e6d5c254e62058f
Witkiewicz-Nikodem <witkiewicz.nikodem@gmail.com>
2026-07-31 01:56:51 +0200

    implemented INTT operation. refactored poly_normal and poly_ntt. now their field is struct Poly which is responsible for polynomial only operators. poly_normal and poly_ntt structs are mainly for distinguishing transformed and no transformed polynomials

--- src/polynomials/polynomials.rs
+++ src/polynomials/polynomials.rs
@@ -1,11 +1,27 @@
+// based on: https://eprint.iacr.org/2024/585.pdf
+
 const N: usize = 4;   // in real 256
+const IN: u32 = 5761;
 const Q: u32 = 7681;    // in real 3329
 const W: u32 = 3383;
+const IW: u32 = 4298; // inverse of W mod Q
 
 const NTT: [[u32; N]; N] = [[1, 1   , 1   , 1   ],
                             [1, 3383, 7680, 4298],
                             [1, 7680, 1   , 7680],
                             [1, 4298, 7680, 3383]]; // well its bigger in reality 256 long.
+
+const INTT: [[u32; N]; N] = [[1, 1   , 1   , 1   ],
+                             [1, 4298, 7680, 3383],
+                             [1, 7680, 1   , 7680],
+                             [1, 3383, 7680, 4298]]; // well its bigger in reality 256 long.
+
+trait Polynomial {
+    fn new(coeffs: [u32; N]) -> Self;
+    fn add(&mut self, other: &Self);
+    fn scalar_mul(&mut self, scalar: u32);
+    fn el_wise_mul(&self, other: &Self) -> Self;
+}
 
 
 
@@ -15,36 +31,107 @@ struct Poly {
 }
 
 #[derive(Clone, Debug)]
-struct PolyNTT {
-    coeffs: [u32; N],
+struct Poly_normal {
+    poly: Poly,
+}
+
+#[derive(Clone, Debug)]
+struct Poly_NTT {
+    poly: Poly,
 }
 
 struct Matrix {
     data: [[u32; N]; N],
 }
 
-
-impl Poly {
+impl Polynomial for Poly {
     fn new(coeffs: [u32; N]) -> Self {
         Self { coeffs }
     }
 
-    fn add(&mut self, other: &Poly){
+    fn add(&mut self, other: &Self) {
         for i in 0..N {
             self.coeffs[i] = (self.coeffs[i] + other.coeffs[i]) % Q;
         }
     }
 
-    fn to_ntt(&self, matrix: &Matrix) -> PolyNTT {
-        let NTT_matrix = Matrix::new(NTT);
-        let result = NTT_matrix.multiply_poly(self);
-        PolyNTT::new(result.coeffs)
+    fn scalar_mul(&mut self, scalar: u32) {
+        for i in 0..N {
+            self.coeffs[i] = (self.coeffs[i] * scalar) % Q;
+        }
+    }
+
+    fn el_wise_mul(&self, other: &Self) -> Self {
+        let mut result = self.clone();
+        for i in 0..N {
+            result.coeffs[i] = (result.coeffs[i] * other.coeffs[i]) % Q;
+        }
+        result
     }
 }
 
-impl PolyNTT {
+
+
+impl Poly_normal {
     fn new(coeffs: [u32; N]) -> Self {
-        Self { coeffs }
+        Self { poly: Poly::new(coeffs) }
+    }
+
+    fn get_coeffs(&self) -> &[u32; N] {
+        &self.poly.coeffs
+    }
+
+    fn add(&mut self, other: &Poly_normal){
+        self.poly.add(&other.poly);
+    }
+    
+    fn scalar_mul(&mut self, scalar: u32){
+        self.poly.scalar_mul(scalar);
+    }
+
+    fn el_wise_mul(&self, other: &Poly_normal) -> Poly_normal {
+        let poly = self.poly.el_wise_mul(&other.poly);
+        Poly_normal { poly }
+    }
+
+    fn convolution(&self, other: &Poly_normal) -> Poly_normal {
+        let a = self.to_ntt();
+        let b = other.to_ntt();
+        let c = a.el_wise_mul(&b);
+        c.to_normal()
+    }
+
+    fn to_ntt(&self) -> Poly_NTT {
+        Poly_NTT::new(Matrix::new(NTT).multiply_poly(self.get_coeffs()))
+    }
+}
+
+impl Poly_NTT {
+    fn new(coeffs: [u32; N]) -> Self {
+        Self { poly: Poly::new(coeffs) }
+    }
+
+    fn get_coeffs(&self) -> &[u32; N] {
+        &self.poly.coeffs
+    }
+
+    fn add(&mut self, other: &Poly_normal){
+        self.poly.add(&other.poly);
+    }
+    
+    fn scalar_mul(&mut self, scalar: u32){
+        self.poly.scalar_mul(scalar);
+    }
+
+    fn el_wise_mul(&self, other: &Poly_NTT) -> Poly_NTT {
+        let poly = self.poly.el_wise_mul(&other.poly);
+        Poly_NTT { poly }
+    }
+
+    fn to_normal(&self) -> Poly_normal {
+        let mut poly = Poly_normal::new(Matrix::new(INTT).multiply_poly(self.get_coeffs()));
+        poly.scalar_mul(IN);
+        poly
     }
 }
 
@@ -54,20 +141,20 @@ impl Matrix {
         Self { data }
     }
 
-    fn multiply_poly(&self, poly: &Poly) -> Poly {
+    fn multiply_poly(&self, poly: &[u32; N]) -> [u32; N] {
         let mut result = [0u32; N];
         for i in 0..N {
             for j in 0..N {
-                result[i] = result[i] + self.data[i][j] * poly.coeffs[j];
+                result[i] = result[i] + self.data[i][j] * poly[j];
             }
             result[i] %= Q;
         }
-        Poly::new(result)
+        result
     }
 }
 
 // just for checking
-pub fn NTT_matrix() -> Matrix {
+pub fn ntt_matrix() -> Matrix {
     let mut data = [[0u32; N]; N];
     for i in 0..N {
         for j in 0..N {
@@ -76,6 +163,17 @@ pub fn NTT_matrix() -> Matrix {
     }
     Matrix::new(data)
 }
+
+pub fn intt_matrix() -> Matrix {
+    let mut data = [[0u32; N]; N];
+    for i in 0..N {
+        for j in 0..N {
+            data[i][j] = mod_pow(IW, (i * j) as u32, Q);
+        }
+    }
+    Matrix::new(data)  
+}
+
 
 pub fn mod_pow(base: u32, exp: u32, modulus: u32) -> u32 {
     let mut result = 1;
@@ -100,18 +198,18 @@ mod tests{
 
     #[test]
     fn test_poly_add() {
-        let mut Poly = Poly::new([1, 2, 3, 4]);
-        let Poly2 = Poly::new([4, 3, 2, 1]);
-        Poly.add(&Poly2);
-        assert_eq!(Poly.coeffs, [5, 5, 5, 5]);
+        let mut poly = Poly_normal::new([1, 2, 3, 4]);
+        let poly2 = Poly_normal::new([4, 3, 2, 1]);
+        poly.add(&poly2);
+        assert_eq!(poly.poly.coeffs, [5, 5, 5, 5]);
     }
 
     #[test]
     fn test_poly_add_mod() {
-        let mut Poly = Poly::new([1 + 7680, 2 + 7680, 3 + 7680, 4 + 7680]);
-        let Poly2 = Poly::new([4, 3, 2, 1]);
-        Poly.add(&Poly2);
-        assert_eq!(Poly.coeffs, [4, 4, 4, 4]);
+        let mut poly = Poly_normal::new([1 + 7680, 2 + 7680, 3 + 7680, 4 + 7680]);
+        let poly2 = Poly_normal::new([4, 3, 2, 1]);
+        poly.add(&poly2);
+        assert_eq!(poly.get_coeffs(), &[4, 4, 4, 4]);
     }
 
     #[test]
@@ -122,9 +220,15 @@ mod tests{
     }
 
     #[test]
-    fn test_eval_NTT_matrix() {
-        let matrix = NTT_matrix();
+    fn test_eval_ntt_matrix() {
+        let matrix = ntt_matrix();
         assert_eq!(matrix.data, NTT);
+    }
+
+    #[test]
+    fn test_eval_intt_matrix(){
+        let matrix  = intt_matrix();
+        assert_eq!(matrix.data, INTT);
     }
 
     #[test]
@@ -133,17 +237,41 @@ mod tests{
                                   [1, 3383, 7680, 4298],
                                   [1, 7680, 1   , 7680],
                                   [1, 4298, 7680, 3383]]);
-        let poly = Poly::new([1, 2, 3, 4]);
+        let poly = [1, 2, 3, 4];
         let result = matrix.multiply_poly(&poly);
-        assert_eq!(result.coeffs, [10, 913, 7679, 6764]);
+        assert_eq!(result, [10, 913, 7679, 6764]);
     }
 
     #[test]
     fn test_poly_to_ntt() {
-        let poly = Poly::new([1, 2, 3, 4]);
-        let matrix = Matrix::new(NTT);
-        let poly_ntt = poly.to_ntt(&matrix);
-        assert_eq!(poly_ntt.coeffs, [10, 913, 7679, 6764]);
+        let poly = Poly_normal::new([1, 2, 3, 4]);
+        let poly_ntt = poly.to_ntt();
+        assert_eq!(poly_ntt.get_coeffs(), &[10, 913, 7679, 6764]);
     }
+
+    #[test]
+    fn test_poly_ntt_to_normal() {
+        let poly = Poly_NTT::new([10, 913, 7679, 6764]);
+        let poly_normal = poly.to_normal();
+        assert_eq!(poly_normal.get_coeffs(), &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn test_poly_transforma_reduction() {
+        let poly = Poly_normal::new([1, 2, 3, 4]);
+        let poly_ntt = poly.to_ntt();
+        let poly_normal = poly_ntt.to_normal();
+        assert_eq!(poly_normal.get_coeffs(), &[1, 2, 3, 4]);
+    }
+
+    // evaluating convolution of two polynomials
+    #[test]
+    fn test_convolution_with_ntt(){
+        let g = Poly_normal::new([1, 2, 3, 4]);
+        let h = Poly_normal::new([5, 6, 7, 8]);
+        let conv = g.convolution(&h);
+        assert_eq!(conv.get_coeffs(), &[66, 68, 66, 60]);
+    }
+
 
 }
